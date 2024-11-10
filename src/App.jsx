bf8fec0c9d4bd72177db437f1eb6cbc6e5aeebe6
Shahstaa <sh.sabraa@gmail.com>
2024-11-10 15:43:26 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './app.css'
+import NavBar from './components/NavBar';
+import MailboxForm from './components/MailboxForm';
+import MailboxList from './components/MailboxList';
+import MailboxDetails from './components/MailboxDetails';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (mailboxes) => {
+    setMailboxes([...mailboxes, newMailbox]); 
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Mailbox List</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
+
+
---
+++ src/components/MailboxDetails.jsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+
+const MailboxDetails = (props) => {
+    const { mailboxId } = useParams();
+    const selectedBox = props.mailboxes.find(
+      (mailbox) => mailbox._id === Number(mailboxId)
+    );
+    console.log(selectedBox);
+    return (
+        <>
+        </>
+    )
+}
+
+export default MailboxDetails;
+
